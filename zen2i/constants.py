a83094ac7c4c5ada498747bbc2d061be4b7e86bd
Hanaasagi <ambiguous404@gmail.com>
2018-08-20 23:31:58 +0800

    initial commit

---
+++ zen2i/constants.py
@@ -0,0 +1,53 @@
+from frozendict import frozendict
+
+
+KANNUM = frozendict({
+    '〇': 0,
+    '零': 0,
+    '':   1,  # noqa
+    '一': 1,
+    '壱': 1,
+    '二': 2,
+    '弐': 2,
+    '三': 3,
+    '参': 3,
+    '四': 4,
+    '肆': 4,
+    '五': 5,
+    '伍': 5,
+    '六': 6,
+    '陸': 6,
+    '七': 7,
+    '漆': 7,
+    '八': 8,
+    '捌': 8,
+    '九': 9,
+    '玖': 9
+})
+
+KANORDER = frozendict({
+    ''  : 1,  # noqa
+    '十': 10,
+    '拾': 10,
+    '百': 100,
+    '陌': 100,
+    '佰': 100,
+    '千': 1000,
+    '阡': 1000,
+    '仟': 1000
+})
+
+KANORDER_F = frozendict({
+    '':   1,  # noqa
+    '万': 10**4,
+    '萬': 10**4,
+    '億': 10**8,
+    '兆': 10**12
+})
+
+ALL_KANSUUJI = sum(
+    [list(d.keys()) for d in (KANNUM, KANORDER, KANORDER_F)],
+    []
+)
+
+MAX_LOOP = 100
---
+++ zen2i/zen2i.py
@@ -0,0 +1,85 @@
+import re
+from constants import ALL_KANSUUJI, MAX_LOOP, KANNUM, KANORDER_F, KANORDER
+
+
+def zen2i(string):
+    result = kanji2num(string)
+    return zen2han(result)
+
+
+def kanji2num(string):
+    arr = split_kansuuji(string)
+    arr = split_kansuuji_detail(arr)
+    result = convert_kansuuji(arr)
+    return ''.join(map(str, result))
+
+
+def zen2han(string):
+    return string.translate(
+        str.maketrans('０１２３４５６７８９', '0123456789')
+    )
+
+
+# 漢数字のところだけ切り出す
+def split_kansuuji(string):
+    return re.split(rf'([{"".join(ALL_KANSUUJI)}]+)', string)
+
+
+# 位を表す数ではない普通の漢数字が続いていたらわける
+def split_kansuuji_detail(arr):
+    array_tmp = arr.copy()
+    array_result = arr.copy()
+    for _ in range(MAX_LOOP):
+        array_result = split_kansuuji_detail_inner(array_result)
+        if array_result == array_tmp:
+            return array_result
+        else:
+            array_tmp = array_result.copy()
+    return array_result
+
+
+def split_kansuuji_detail_inner(arr):
+    result = []
+    for a in arr:
+        if re.match(rf'([{"".join(ALL_KANSUUJI)}]+)', a):
+            kannum = ''.join(KANNUM.keys())
+            r = re.sub(rf'([{kannum}])([{kannum}])', r'\1,\2', a).split(',')
+            result.extend(r)
+        else:
+            result.append(a)
+    return result
+
+
+# ４桁ごとの単位（万、億、兆など）でまずわけ、
+# それぞれに対して漢数字→数字を実行している。
+def convert_kansuuji(arr):
+    result = []
+    for a in arr:
+        if re.match(rf'[{"".join(ALL_KANSUUJI)}]+', a):
+            reg_order_f = (
+                rf'([^{"".join(KANORDER_F.keys())}]*)'
+                rf'([{"".join(KANORDER_F.keys())}]?)'
+            )
+            match = re.findall(reg_order_f, a)
+            ret = 0
+            for (lt_1000, order_f) in match:
+                reg_order = (
+                    rf'([^{"".join(KANORDER.keys())}]*)'
+                    rf'([{"".join(KANORDER.keys())}]?)'
+                )
+                match2 = re.findall(reg_order, lt_1000)
+                tmp = -1
+                for (_1_9, order) in match2:
+                    tmp = tmp + KANNUM[_1_9] * KANORDER[order]
+                ret = ret + tmp * KANORDER_F[order_f]
+            result.append(ret)
+        else:
+            result.append(a)
+    return result
+
+
+if __name__ == '__main__':
+    assert zen2i('hoge') == 'hoge'
+    assert zen2i('一二三') == '123'
+    assert zen2i('百三') == '103'
+    assert zen2i('三兆五十二万四十八') == '3000000520048'
